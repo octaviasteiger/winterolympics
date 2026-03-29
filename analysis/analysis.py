02479b7142bd637ecfa9087ac5ba1a2690b03475
Octavia Steiger <os459@exeter.ac.uk>
2026-03-29 11:44:26 +0100

    adding in the imports to analysis

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,5 +1,14 @@
 """pip install statsmodels
 and add it to the requirements files
 
+analysis.py 
+Aggregates the completed medal data and runs an OLS regression to test whether host countries experience a 
+measurable performance advantage at the Winter Olympics. 
 """
+
+import pandas as pd
+import numpy as np
+import sqlite3
+import statsmodels.formula.api as smf
+import os
 
